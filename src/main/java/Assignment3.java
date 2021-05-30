fcf0c0a362095a8623d1ba49a9926d9b098d1d3d
TashreeqWilliams <you@example.com>
2021-05-30 13:04:27 +0200

    CodeBase: This should consist of the basic structure

--- src/main/java/Assignment3.java
+++ src/main/java/Assignment3.java
@@ -1,5 +1,9 @@
+
 public class Assignment3 {
-    public static void main (String [] args){
-        System.out.println("Testing 1 , 2");
+    public static void main(String[] args) {
+
     }
+
 }
+
+
