174765096f32b4550d90cd4269ab64495b6278ad
Athenkosi <athenkosizono@gmail.com>
2021-06-01 23:34:29 +0200

    Updated

--- src/main/java/za/ac/cput/Factory/IssueFactory.java
+++ src/main/java/za/ac/cput/Factory/IssueFactory.java
@@ -8,15 +8,5 @@ package za.ac.cput.Factory;
 import za.ac.cput.Entity.Issue;
 
 public class IssueFactory {
-    //String issueId, issueDescription, issueArea, issueRaisedDate, issueResolvedDate;
-    // boolean issueStatus, isResolved, isValidated;
-    public static Issue createIssue(String issueDescription, String issueArea, String issueRaisedDate,
-                                    String issueResolvedDate, boolean issueStatus, boolean isResolved,
-                                    boolean isValidated){
-
-        Issue issue = new Issue.Builder().issueId("").issueDescription(issueDescription).issueArea(issueArea)
-                .issueRaisedDate(issueRaisedDate).issueResolvedDate(issueResolvedDate).issueStatus(issueStatus)
-                .isResolved(isResolved).isValidated(isValidated).Build();
-        return issue;
-    }
+    //Under Construction
 }
