4664817e7f217eb306708752a101696287cec7f8
LindoXaki <215041720>
2021-06-01 23:47:06 +0200

    "Third commit"

---
+++ src/main/java/za/ac/cput/Entity/Auditor.java
@@ -0,0 +1,87 @@
+package za.ac.cput.Entity;
+
+public class Auditor {
+    private String auditorID;
+    private String auditorFirstName;
+    private String auditorSurname;
+    private String cellphone;
+
+
+    private Auditor(Builder builder) {
+        this.auditorID = builder.auditorID;
+        this.auditorFirstName = builder.auditorFirstName;;
+        this.auditorSurname = builder.auditorSurname;;
+        this.cellphone = builder.cellphone;
+    }
+
+    public String getAuditorID() {
+        return auditorID;
+    }
+
+    public String getAuditorFirstName() {
+        return auditorFirstName;
+    }
+
+    public String getAuditorSurname() {
+        return auditorSurname;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Auditor{" +
+                "auditorID='" + auditorID + '\'' +
+                ", auditorFirstName='" + auditorFirstName + '\'' +
+                ", auditorSurname='" + auditorSurname + '\'' +
+                ", cellphone='" + cellphone + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String auditorID;
+        private String auditorFirstName;
+        private String auditorSurname;
+        private String cellphone;
+
+
+        public Builder setAuditorID(String auditorID) {
+            this.auditorID = auditorID;
+            return this;
+        }
+
+        public Builder setAuditorFirstName(String auditorFirstName) {
+            this.auditorFirstName = auditorFirstName;
+            return this;
+        }
+
+        public Builder setAuditorSurname(String auditorSurname) {
+            this.auditorSurname = auditorSurname;
+            return this;
+        }
+
+        public Builder setCellphone(String cellphone) {
+            this.cellphone = cellphone;
+            return this;
+        }
+
+        public Auditor Build() {
+            return new Auditor(this);
+        }
+
+        private Builder copy(Auditor auditor) {
+            this.auditorID = auditor.auditorID;
+            this.auditorFirstName = auditor.auditorFirstName;
+            this.auditorSurname = auditor.auditorSurname;
+            this.cellphone = auditor.cellphone;
+
+            return this;
+
+        }
+
+
+    }
+    }
