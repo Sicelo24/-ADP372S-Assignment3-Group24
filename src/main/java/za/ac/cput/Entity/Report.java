a461004fb204aba441969529ccee433481eb1d37
Mlungisi Xakekile <79154709+Mlungisi-Xakekile@users.noreply.github.com>
2021-06-01 18:15:16 +0200

    Entity

---
+++ src/main/java/za/ac/cput/Entity/Report.java
@@ -0,0 +1,67 @@
+package za.ac.cput.Entity;
+
+
+public class Report {
+    private String reportId, reportAuth, reportDate;
+
+
+    private Report(){}
+
+    private Report( Builder builder) {
+        this.reportId = builder.reportId;
+        this.reportAuth = builder.reportAuth;
+        this.reportDate = builder.reportDate;
+    }
+
+    public String getReportId() {
+        return reportId;
+    }
+
+    public String getReportAuth() {
+        return reportAuth;
+    }
+
+    public String getReportDate() {
+        return reportDate;
+    }
+
+    public java.lang.String toString() {
+        return "Report{" +
+                "reportId=" + reportId +
+                ", reportAuth=" + reportAuth +
+                ", reportDate=" + reportDate +
+                '}';
+    }
+
+    public static class Builder {
+        private String reportId, reportAuth, reportDate;
+
+        public Builder setReportId(String reportId) {
+            this.reportId = reportId;
+            return this;
+        }
+
+        public Builder setReportAuth(String reportAuth) {
+            this.reportAuth = reportAuth;
+            return this;
+        }
+
+        public Builder setReportDate(String reportDate) {
+            this.reportDate = reportDate;
+            return this;
+        }
+
+        public Builder copy(Report report) {
+            this.reportId = report.reportId;
+            this.reportAuth = report.reportAuth;
+            this.reportDate = report.reportDate;
+            return this;
+        }
+
+        public Report build() {
+            return new Report(this);
+
+        }
+    }
+}
+
