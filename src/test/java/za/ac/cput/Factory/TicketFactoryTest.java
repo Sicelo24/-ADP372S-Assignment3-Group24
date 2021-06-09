29878f6bf4ef817ed154a6828f9baaed8e34c468
Sicelo24 <sicelozitha@gmail.com>
2021-06-09 23:43:10 +0200

    Ticket Factory Test added

---
+++ src/test/java/za/ac/cput/Factory/TicketFactoryTest.java
@@ -0,0 +1,32 @@
+/*
+        @Author : Sicelo Zitha 216140943
+        TicketFactoryTest class
+        date 6 June 2021
+
+*/
+package za.ac.cput.Factory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.Entity.Ticket;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketFactoryTest {
+    Ticket ticket;
+
+    @BeforeEach
+    void setUp() {
+        ticket = TicketFactory.buildTicket("2",
+                "Student card complaint",
+                "10/03/2020",
+                "7");
+    }
+    @Test
+    void buildUser(){
+        assertNotNull(ticket);
+        System.out.println(ticket);
+    }
+
+}
