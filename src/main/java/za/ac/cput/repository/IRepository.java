544bfbdfe53e2dc2131c3b08c8fb6673861371ea
TashreeqWilliams <you@example.com>
2021-07-22 13:48:11 +0200

    Repository and it's test case

---
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -0,0 +1,23 @@
+package za.ac.cput.repository;
+
+public class IRepository {
+
+    public  void create(){
+
+    }
+
+    public void read(){
+
+    }
+
+    public void update(){
+
+    }
+    public void delete(){
+
+
+    }
+
+}
+
+
