e65ab81ebca68a1757235bdb5597da3dbd2ca1e4
Athenkosi <athenkosizono@gmail.com>
2021-07-25 04:55:33 +0200

    Added IssueRepository Test Class

---
+++ src/test/java/za/ac/cput/repository/IssueRepositoryTest.java
@@ -0,0 +1,33 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IssueRepositoryTest {
+
+    @BeforeEach void setup(){
+
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
