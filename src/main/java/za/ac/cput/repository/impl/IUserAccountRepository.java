a3a4d6cf9fc997fbea6b598bd316a5753efe1e1f
TashreeqWilliams <you@example.com>
2021-07-28 20:08:37 +0200

    UserAccount Repository

---
+++ src/main/java/za/ac/cput/repository/impl/IUserAccountRepository.java
@@ -0,0 +1,14 @@
+package za.ac.cput.repository.impl;
+/*
+ User Account Rep
+ Author: Tashreeq Williams (217290671)
+ Date: 26 July 2021
+*/
+
+import za.ac.cput.entity.UserAccount;
+import za.ac.cput.repository.IRepository;
+import java.util.Set;
+
+public interface IUserAccountRepository extends IRepository<UserAccount,String>{
+    Set<UserAccount> getAll();
+}
---
+++ src/main/java/za/ac/cput/repository/impl/UserAccountRepository.java
@@ -0,0 +1,61 @@
+package za.ac.cput.repository.impl;
+/*
+ User Account Rep
+ Author: Tashreeq Williams (217290671)
+ Date: 26 July 2021
+*/
+import za.ac.cput.entity.UserAccount;
+import java.util.*;
+import java.util.Set;
+
+public class UserAccountRepository implements IUserAccountRepository {
+    private static UserAccountRepository userAccountRepository = null;
+    private Set<UserAccount> userAccountsDB;
+
+    private UserAccountRepository() {
+        userAccountsDB = new HashSet<>();
+    }
+
+    public static UserAccountRepository createUserAccountRepository() {
+        if (userAccountRepository == null) userAccountRepository = new UserAccountRepository();
+        return userAccountRepository;
+    }
+
+    @Override
+    public Set<UserAccount> getAll() {
+        return userAccountsDB;
+    }
+
+    @Override
+    public UserAccount create(UserAccount userAccount) {
+        userAccountsDB.add(userAccount);
+        return userAccount;
+    }
+
+    @Override
+    public UserAccount read(String s) {
+        for (UserAccount userAccount : userAccountsDB) {
+            return userAccount;
+        }
+        return null;
+    }
+
+    @Override
+    public UserAccount update(UserAccount userAccount) {
+        UserAccount r = create(userAccount);
+        if (r != null)
+            this.userAccountsDB.remove(r);
+        this.userAccountsDB.add(userAccount);
+        return userAccount;
+    }
+
+    @Override
+    public boolean delete(String s) {
+        UserAccount userAccount = read(s);
+        if (userAccount != null) {
+            this.userAccountsDB.remove(userAccount);
+            return true;
+        } else
+            return false;
+    }
+}
