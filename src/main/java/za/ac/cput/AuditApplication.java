ec53dcf78d4556002a0ddcc3b57a8aa103fb33a3
Athenkosi <athenkosizono@gmail.com>
2021-08-04 04:31:41 +0200

    Initial Setup Added

--- src/main/java/za/ac/cput/AuditApplication.java
+++ src/main/java/za/ac/cput/AuditApplication.java
@@ -1,4 +1,10 @@
 package za.ac.cput;
 
+import org.springframework.boot.SpringApplication;
+import za.ac.cput.controller.IssueController;
+
 public class AuditApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IssueController.class,args);
+    }
 }
