3ca47461cf7197385227048705e0d65633471b51
Athenkosi <athenkosizono@gmail.com>
2021-08-04 03:01:06 +0200

    Added Controller Package

---
+++ src/main/java/za/ac/cput/controller/IssueController.java
@@ -0,0 +1,10 @@
+/* IssueController.java Class
+ * Entity for Issue Controller
+ * Author: Athenkosi Zono (218030185)
+ * Date: 4 August 2021
+ */
+
+package za.ac.cput.controller;
+
+public class IssueController {
+}
