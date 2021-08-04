fc875588a20167ed9f48124dc8548cd8e3ac8967
Athenkosi <athenkosizono@gmail.com>
2021-08-04 05:01:05 +0200

    @Service Annotation added

--- src/main/java/za/ac/cput/service/impl/IssueService.java
+++ src/main/java/za/ac/cput/service/impl/IssueService.java
@@ -6,11 +6,12 @@
 
 package za.ac.cput.service.impl;
 
+import org.springframework.stereotype.Service;
 import za.ac.cput.entity.Issue;
 import za.ac.cput.repository.entity.IssueRepository;
-
 import java.util.Set;
 
+@Service
 public class IssueService implements IIssueService{
 
     private static IssueService issueService = null;
