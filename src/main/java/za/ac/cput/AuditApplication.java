e22cced1f6135494dcd3af99449275ba766d9bcd
Athenkosi <athenkosizono@gmail.com>
2021-09-27 10:30:35 +0200

    Home Controller Added

--- src/main/java/za/ac/cput/AuditApplication.java
+++ src/main/java/za/ac/cput/AuditApplication.java
@@ -1,7 +1,6 @@
 package za.ac.cput;
 
 import org.springframework.boot.SpringApplication;
-import za.ac.cput.controller.IssueController;
 
 public class AuditApplication {
     public static void main(String[] args) {
